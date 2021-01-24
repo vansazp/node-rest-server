dcc7029a17d627ecf6910b2882ae58fc3c2404e3
Iván Sanz <ivansanz@joumma.com>
2021-01-24 17:31:27 +0100

    validaciones en el put con underscore

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const _ = require('underscore');
 
 const Usuario = require('../models/usuario');
 const usuario = require('../models/usuario');
@@ -39,9 +40,9 @@ app.post('/usuario', function(req, res) {
 
 app.put('/usuario/:id', function(req, res) {
     let id = req.params.id;
-    let body = req.body;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
-    Usuario.findByIdAndUpdate(id, body, { new: true }, (err, usuarioDB) => {
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
         if (err) {
             return res.status(400).json({
                 ok: false,
